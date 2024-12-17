b9512555eec6cafde04508acb6b91bd246a4197e
Andres Moreno <amorenompls@gmail.com>
2024-12-16 18:05:07 -0800

    Incantation for calling the promise in ttt.scm

--- ttt.js
+++ ttt.js
@@ -1,9 +1,10 @@
-window.addEventListener("load", function() {
-  Scheme.load_main("ttt.wasm", {
+window.addEventListener("load", async ()  => {
+  const [proc] = await Scheme.load_main("ttt.wasm", {
     user_imports: {
-      window: {
+      core: {
         log(text) { console.log(text); }
       }
     }
   });
+  proc.call_async();
 });
--- ttt.scm
+++ ttt.scm
@@ -1,12 +1,10 @@
 (use-modules (scheme write)
              (hoot ffi)
              (fibers)
-             (fibers promises)
-             (fibers timers))
-
+             (fibers promises))
 
 (define-foreign console-log
-  "window" "log"
+  "core" "log"
   (ref string) -> (ref null extern))
 
 (console-log "Before promise!")
@@ -15,6 +13,5 @@
   (call-with-async-result
    resolved rejected
    (lambda ()
-     (console-log "entered promise..."))))
-
-(console-log "After promise!")
+     (console-log "entered promise...")
+     42)))
